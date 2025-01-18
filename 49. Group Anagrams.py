673845e954a1b4ec0904f66c980f7c2bfbe68b76
Vibhaw Kumar <85498120+nik-vib@users.noreply.github.com>
2025-01-18 18:46:12 +0530

    Update 49. Group Anagrams.py

--- 49. Group Anagrams.py
+++ 49. Group Anagrams.py
@@ -1,24 +1,10 @@
 class Solution:
-    def areAnagrams(self, word1: str, word2: str) -> bool:
-        # Function to check if two words are anagrams
-        return sorted(word1) == sorted(word2)
-
-    def groupAnagrams(self, words: List[str]) -> List[List[str]]:
-        # List to store groups of anagrams
-        anagram_groups = []
-
-        # Iterate through each word in the input list
-        for word in words:
-            # Check if the word is already assigned to an existing group
-            assigned = False
-            for group in anagram_groups:
-                if self.areAnagrams(word, group[0]):
-                    group.append(word)
-                    assigned = True
-                    break
-            
-            # If the word is not assigned, create a new group
-            if not assigned:
-                anagram_groups.append([word])
-
-        return anagram_groups
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dic={}
+        for word in strs:
+            sorted_word="".join(sorted(word))
+            if sorted_word not in dic:
+                dic[sorted_word]=[word]
+            else:
+                dic[sorted_word].append(word)
+        return dic.values()
