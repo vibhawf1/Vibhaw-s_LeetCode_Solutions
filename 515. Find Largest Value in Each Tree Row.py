13323a44899364cbf3dbff70a9fd345e87625a8e
Vibhaw Kumar <85498120+nik-vib@users.noreply.github.com>
2025-01-21 18:23:41 +0530

    Create 515. Find Largest Value in Each Tree Row.py

---
+++ 515. Find Largest Value in Each Tree Row.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def largestValues(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+
+        max_values = []
+        queue = deque([root])
+
+        while queue:
+            level_max = float('-inf')
+            level_size = len(queue)
+
+            for _ in range(level_size):
+                node = queue.popleft()
+                level_max = max(level_max, node.val)
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+            max_values.append(level_max)
+
+        return max_values
